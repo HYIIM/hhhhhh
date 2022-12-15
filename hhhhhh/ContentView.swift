6b101ff4c67c61ac209a14082eb6a7d82d699cdb
HYIIM <Haya.m.alsa@gmail.com>
2022-12-15 10:04:57 +0300

    Initial Commit

---
+++ hhhhhh/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  hhhhhh
+//
+//  Created by HY M on 21/05/1444 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ hhhhhh/hhhhhhApp.swift
@@ -0,0 +1,17 @@
+//
+//  hhhhhhApp.swift
+//  hhhhhh
+//
+//  Created by HY M on 21/05/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct hhhhhhApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
